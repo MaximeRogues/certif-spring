a32499afbe2c541ddd01e9f56c5152ee768b5dba
MaximeRogues <maximerogues1997@gmail.com>
2024-10-14 21:57:58 +0200

    Ajout du POST et GET avec pagination

--- src/main/java/com/maximerogues/cashcard/CashCardController.java
+++ src/main/java/com/maximerogues/cashcard/CashCardController.java
@@ -1,11 +1,14 @@
 package com.maximerogues.cashcard;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -26,5 +29,28 @@ public class CashCardController {
         } else {
             return ResponseEntity.notFound().build();
         }
+        // OU return cashCardOptional.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    private ResponseEntity<CashCard> create(@RequestBody CashCard newCashCard) {
+        CashCard savedCashCard = cashCardRepository.save(newCashCard);
+        return ResponseEntity.created(URI.create("/cashcards/" + savedCashCard.id())).build();
+    }
+
+//    @GetMapping()
+//    private ResponseEntity<Iterable<CashCard>> findAll() {
+//        return ResponseEntity.ok(cashCardRepository.findAll());
+//    }
+
+    @GetMapping
+    private ResponseEntity<List<CashCard>> findAll(Pageable pageable) {
+        Page<CashCard> page = cashCardRepository.findAll(
+                PageRequest.of(
+                        pageable.getPageNumber(), // 0 par défaut
+                        pageable.getPageSize(),   // 20 par défaut (fonctionnement de Spring Web)
+                        pageable.getSortOr(Sort.by(Sort.Direction.ASC, "amount"))
+                ));
+        return ResponseEntity.ok(page.getContent());
     }
 }
